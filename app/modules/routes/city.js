402b3b2b6d5f9871567d3b4763fa8484ac18742b
Llimaa <marcoslimadefatima@gmail.com>
2020-08-27 08:45:56 -0300

    nova rota para city

--- app/modules/routes/city.js
+++ app/modules/routes/city.js
@@ -1,14 +1,17 @@
 module.exports = app => {
-    const url = `${app.config.url}/city`
+    const url = `${app.config.url}/`
     const Controller = require('../controllers/city')(app)
 
-    app.route(`${url}/find`)
+    app.route(`${url}city/find`)
         .get(Controller.find)
 
-    app.route(`${url}`)
+    app.route(`${url}:id/city`)
+    .get(Controller.listAllOrderByState)
+    
+    app.route(`${url}city`)
         .post(Controller.create)
         .put(Controller.update)
 
-    app.route(`${url}/:id`)
+    app.route(`${url}city/:id`)
         .delete(Controller.delete)
 }
